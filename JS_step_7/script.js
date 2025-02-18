45bcb14c336ef48e3362ea6b9b17575e725d5428
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-02-18 18:39:39 +0200

    Update step 7

--- JS_step_7/script.js
+++ JS_step_7/script.js
@@ -44,13 +44,24 @@ function showProgrammingLangs(plan) {
 showProgrammingLangs(personalPlanPeter);
 
 // ============================================
-const family = ['Peter', 'Ann', 'Alex', 'Linda', 'Maksym'];
+const family = ["Peter", "Ann", "Alex", "Linda", "Maksym"];
 
 function showFamily(arr) {
-    if (family.length === 0) {
-        return 'Семья пуста' 
-    } else {
-        return 'Семья состоит из: ' + family.join(' ');
-    }
+  if (family.length === 0) {
+    return "Семья пуста";
+  } else {
+    return "Семья состоит из: " + family.join(" ");
+  }
 }
 console.log(showFamily(family));
+
+// =============================================
+const favoriteCities = ["liSBon", "ROME", "miLan", "Dublin"];
+
+function standardizeStrings(arr) {
+  arr.forEach((city) => {
+    console.log(city.toLowerCase());
+  });
+}
+
+standardizeStrings(favoriteCities);
