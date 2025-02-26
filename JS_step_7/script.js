a8b2858139e9585d91faec120c8d1a38402dc5e9
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-02-26 14:43:05 +0200

    Add script

--- JS_step_7/script.js
+++ JS_step_7/script.js
@@ -65,3 +65,44 @@ function standardizeStrings(arr) {
 }
 
 standardizeStrings(favoriteCities);
+
+// ====================================================
+const someString = "This is some strange string";
+
+function reverse(str) {
+  if (typeof str !== "string") {
+    return "Ошибка!";
+  }
+  // Самый оптимальный вариант решения
+  return str.split("").reverse().join("");
+}
+console.log(reverse(someString));
+
+// ======================================================
+const baseCurrencies = ["USD", "EUR"];
+const additionalCurrencies = ["UAH", "RUB", "CNY"];
+
+function availableCurr(arr, missingCurr) {
+  let str = "";
+  arr.length === 0
+    ? (str = "Нет доступных валют")
+    : (str = "Доступные валюты:\n");
+
+  arr.forEach(function (curr, i) {
+    if (curr !== missingCurr) {
+      str += `${curr}\n`;
+    }
+  });
+
+  // Или
+  // for (let i = 0; i < arr.length; i++) {
+  //     if (arr[i] === missingCurr) {
+  //         continue;
+  //     }
+  //     str += `${arr[i]}\n`;
+  // }
+
+  return str;
+}
+
+availableCurr([...baseCurrencies, ...additionalCurrencies], "CNY");
