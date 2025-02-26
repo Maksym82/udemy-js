c0cd4c9741e9c39381f7125dcf2390497c6e4def
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-02-26 19:48:00 +0200

    Update step_7

--- JS_step_7/script.js
+++ JS_step_7/script.js
@@ -106,3 +106,45 @@ function availableCurr(arr, missingCurr) {
 }
 
 availableCurr([...baseCurrencies, ...additionalCurrencies], "CNY");
+
+// ====================================================
+const students = [
+  "Peter",
+  "Andrew",
+  "Ann",
+  "Mark",
+  "Josh",
+  "Sandra",
+  "Cris",
+  "Bernard",
+  "Takesi",
+  "Sam",
+  "Yana"
+];
+
+function sortStudentsByGroups(arr) {
+  arr.sort();
+  const a = [],
+    b = [],
+    c = [],
+    rest = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i < 3) {
+      a.push(arr[i]);
+    } else if (i < 6) {
+      b.push(arr[i]);
+    } else if (i < 9) {
+      c.push(arr[i]);
+    } else {
+      rest.push(arr[i]);
+    }
+  }
+  return [
+    a,
+    b,
+    c,
+    `Оставшиеся студенты: ${rest.length === 0 ? "-" : rest.join(", ")}`,
+  ];
+}
+console.log(sortStudentsByGroups(students));
